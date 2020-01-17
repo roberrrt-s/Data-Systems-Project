cf538804b52142d99dd414a404aa14d7b4db1112
Robert Spier <robert.spier@gmail.com>
2020-01-17 15:57:22 +0100

    Update data location + axios + gitignore

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,10 +1,21 @@
 // Core
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
+import composeDynamicImport from 'react-simple-async-import';
 import PropTypes from 'prop-types';
 
 import Main from 'scripts/partials/Main';
 import Menu from 'scripts/partials/Menu';
+
+
+const options = {
+	load: () => import('./data/data.json'),
+	refresh: module.hot && (load => module.hot.accept('data.json', load)),
+	placeholder: <p>test</p>,
+};
+
+const data = composeDynamicImport(options);
+console.log(data);
 
 class App extends Component {
 	constructor() {
